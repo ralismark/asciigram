ce8e9f1e0457e33874b0241a2b3bc32c06d5bb47
ralismark <timmy.yao@outlook.com.au>
2017-11-09 12:40:50 +1100

    Add text block

--- item/text.hpp
+++ item/text.hpp
@@ -25,3 +25,31 @@ public:
 		y += oy;
 	}
 };
+
+struct TextBlock
+	: public Drawable
+{
+	std::vector<std::string> block;
+	int x, y;
+public:
+	TextBlock(int x, int y)
+		: block(), x(x), y(y)
+	{
+	}
+
+	virtual void draw(Canvas& canvas) const override
+	{
+		int ypos = y;
+		for(auto& row : block) {
+			canvas.direct(row, x, ypos);
+			++ypos;
+		}
+	}
+
+	virtual void shift(int ox, int oy) override
+	{
+		x += ox;
+		y += oy;
+		mvprintw(3, 3, "Shift %d %d", ox, oy);
+	}
+};
