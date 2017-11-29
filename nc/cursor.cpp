79c61647d88e8e3b3ac7859954fec4f15913c6a5
ralismark <timmy.yao@outlook.com.au>
2017-11-29 22:42:05 +1100

    Move OwnerFinder to implementation file

--- nc/cursor.cpp
+++ nc/cursor.cpp
@@ -3,6 +3,26 @@
 point cur;
 point region;
 
+struct OwnerFinder
+	: public Canvas
+{
+	int current_id;
+	int target_id;
+	int tx, ty;
+public:
+	OwnerFinder(int x, int y)
+		: current_id(-1), target_id(-1), tx(x), ty(y)
+	{
+	}
+
+	virtual void impl_set(char /* fill */, int x, int y) override
+	{
+		if(x == tx && y == ty) {
+			target_id = current_id;
+		}
+	}
+};
+
 int idhere()
 {
 	OwnerFinder of(cur.x, cur.y);
--- nc/cursor.hpp
+++ nc/cursor.hpp
@@ -10,25 +10,5 @@
 extern point cur;
 extern point region;
 
-struct OwnerFinder
-	: public Canvas
-{
-	int current_id;
-	int target_id;
-	int tx, ty;
-public:
-	OwnerFinder(int x, int y)
-		: current_id(-1), target_id(-1), tx(x), ty(y)
-	{
-	}
-
-	virtual void impl_set(char /* fill */, int x, int y) override
-	{
-		if(x == tx && y == ty) {
-			target_id = current_id;
-		}
-	}
-};
-
 int idhere();
 std::set<int> id_in_region(int x1, int y1, int x2, int y2);
