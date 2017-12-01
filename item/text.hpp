75e39918e1d5d55634e9f1a0082856c97f35fe36
ralismark <timmy.yao@outlook.com.au>
2017-12-01 21:44:56 +1100

    Document Drawable items
    This documents the currently available non-aggregate Drawables:
    - Text
    - Box
    - Arrow

--- item/text.hpp
+++ item/text.hpp
@@ -1,8 +1,19 @@
 #pragma once
 
+/**
+ * \file
+ * This file defines Text, a Drawable which represents a block of raw text.
+ */
+
 #include "../canvas.hpp"
 #include "../drawable.hpp"
 
+/**
+ * Stores a block of text.
+ *
+ * This type is able to store a string, which may contain line endings. When
+ * rendering, this is handled correctly, aligning them to the left.
+ */
 struct Text
 	: public Drawable
 {
@@ -14,11 +25,17 @@ public:
 	{
 	}
 
+	/**
+	 * Create a copy.
+	 */
 	virtual std::unique_ptr<Drawable> clone() const
 	{
 		return std::make_unique<Text>(*this);
 	}
 
+	/**
+	 * Draw the text, correctly handling newlines.
+	 */
 	virtual void draw(Canvas& canvas) const override
 	{
 		int line_y = y;
@@ -33,6 +50,9 @@ public:
 		} while(start != std::string::npos + 1);
 	}
 
+	/**
+	 * Offset the element by a (\p ox, \p oy).
+	 */
 	virtual void shift(int ox, int oy) override
 	{
 		x += ox;
